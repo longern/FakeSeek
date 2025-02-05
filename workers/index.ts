86d7c9cb0e73c597c68faea6621d00ba8b057560
longern <i@longern.com>
2025-02-05 12:03:14 +0800

    Add Google search API

--- workers/index.ts
+++ workers/index.ts
@@ -1,5 +1,23 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello World!");
-  },
-};
+import { Hono } from "hono";
+
+const app = new Hono<{
+  Bindings: {
+    GOOGLE_API_KEY: string;
+    GOOGLE_CSE_CX: string;
+  };
+}>();
+
+app.get("/api/search", (c) => {
+  const query = c.req.query("q");
+  if (!query)
+    return Response.json({ error: "Missing query parameter" }, { status: 400 });
+
+  const params = new URLSearchParams({
+    q: query,
+    cx: c.env.GOOGLE_CSE_CX,
+    key: c.env.GOOGLE_API_KEY,
+  });
+  return fetch(`https://www.googleapis.com/customsearch/v1?${params}`);
+});
+
+export default app;
